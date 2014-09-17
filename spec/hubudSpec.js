cbdbf17d663c797e71a5eb1435224c3ec1888a43
NineInchNade <nineinchnade@gmail.com>
2014-09-17 11:22:20 +0100

    added first test

---
+++ spec/hubudSpec.js
@@ -0,0 +1,12 @@
+describe("hubud", function() {
+
+	describe("api token", function() {
+		
+		it("has an api token", function() {
+			expect(API_TOKEN).toBe("49bf3d1c4360a29e76973028ae09571d89250153"); 
+
+		});
+	});
+
+
+});
